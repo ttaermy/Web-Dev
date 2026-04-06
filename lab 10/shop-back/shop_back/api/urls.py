bfb104ae9257e4719c8712461243ff091204bc46
ttaermy <tomiristulebaeva37@gmail.com>
2026-04-07 02:22:20 +0500

    lab 10

---
+++ lab 10/shop-back/shop_back/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', ProductDetailAPIView.as_view()),
+
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/', CategoryDetailAPIView.as_view()),
+    path('categories/<int:category_id>/products/', CategoryProductsAPIView.as_view()),
+]
---
+++ lab 10/shop-back/shop_back/api/views.py
@@ -0,0 +1,71 @@
+from django.http import JsonResponse
+from .models import Product, Category
+
+def products_list(request):
+    products = Product.objects.all()
+    data = []
+
+    for p in products:
+        data.append({
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category': p.category.id
+        })
+
+    return JsonResponse(data, safe=False)
+
+
+
+def product_detail(request, id):
+    try:
+        p = Product.objects.get(id=id)
+        data = {
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category': p.category.id
+        }
+        return JsonResponse(data)
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product not found'}, status=404)
+    
+
+
+def categories_list(request):
+    categories = Category.objects.all()
+    data = [{'id': c.id, 'name': c.name} for c in categories]
+    return JsonResponse(data, safe=False)
+
+
+
+def category_detail(request, id):
+    try:
+        c = Category.objects.get(id=id)
+        return JsonResponse({'id': c.id, 'name': c.name})
+    except Category.DoesNotExist:
+        return JsonResponse({'error': 'Category not found'}, status=404)
+    
+
+
+def category_products(request, id):
+    products = Product.objects.filter(category_id=id)
+
+    data = []
+    for p in products:
+        data.append({
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+        })
+
+    return JsonResponse(data, safe=False)
